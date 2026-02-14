d92b14a6fdd107bd9fc46f375edebe783cfc12d8
Nithin Shivanna <nithin.s@opqbootcamp.com>
2026-02-14 11:44:05 +0530

    Update notes.

---
+++ javascript/Class2_Fundamentals_JS/hello.js
@@ -0,0 +1,105 @@
+console.log("Hello World!")
+let pencilPrice = 20;
+let eraserPrice = 10;
+
+// console.log("The total price of prurchase " + (pencilPrice+eraserPrice)  + " Rupees.")
+// console.log(`The total price of prurchase  ${ (pencilPrice+eraserPrice) } Rupees.`)
+
+// let x = 'a';
+// let y = 'A';
+
+// console.log(x > y)
+
+// let age = 19;
+
+// if( age >= 18 )
+// {
+//     console.log("he is eligible for voting.");
+//     console.log("he can drive");
+//     let b = 10;
+// }
+// else 
+// {
+//     console.log('He is minor.')
+// }
+
+
+let color = "green"
+
+// if(color == "red")
+// {
+//     console.log("stop")
+// }
+// else if(color == "yellow")
+// {
+//     console.log("slowdown")
+// }
+// else if( color == "green")
+// {
+//     console.log("Go")
+// }
+// else{
+//     console.log("invalid color")
+// }
+
+// switch(color)
+// {
+//     case 'red':
+//             console.log("stop");
+//             break;
+//     case 'yellow':
+//             console.log('slowdown');
+//             break;
+//     case 'green':
+//             console.log('Go');
+//             break;
+//     default:
+//             console.log('Invalid color');
+
+// }
+
+// let size = 'L';
+ 
+// if (size == 'XL') {
+//     console.log("price is rs.250");
+// } else if (size == 'L') {
+//     console.log("price is rs.200");
+// } else if (size == 'M') {
+//     console.log("price is rs.100");
+// } else if (size == 'S') {
+//     console.log("price is rs.50");
+// } else {
+//     console.log("Size not found.");
+// }
+ 
+
+// let score = 33
+
+// if(score > 35)
+// {
+//     if(score > 80)
+//     {
+//         console.log('A Grade.')
+//     }
+//     else
+//     {
+//         console.log("B grade")
+//     }
+// }
+// else
+// {
+//     console.log("Better luck next time")
+// }
+
+
+let text = "helicopter";
+
+
+if( (text[0] == 'a') && (text.length > 3) )
+{
+    console.log("good");
+}
+else
+{
+     console.log("bad");
+}
